bd71d8f462c69a74fb81bfdd9c638f477c330f63
Tobias Toland <github@thinktt.e4ward.com>
2020-10-31 14:59:24 -0500

    finally getting sizing to work

--- main.js
+++ main.js
@@ -2,38 +2,24 @@ const canvas = document.querySelector('#canvas')
 const fullScreenButton = document.querySelector('#fullScreen')
 const startButton = document.querySelector('#start')
 
-// navigator.getGamepads_ = navigator.getGamepads;
-// navigator.getGamepads = function() {
-//   var gamepadlist = navigator.getGamepads_();
-//   var pads = [];
-//   for (var i = 0; i < gamepadlist.length; i++) {
-//     pads[i] = (gamepadlist[i] !== null ? gamepadlist[i] : undefined);
-//   }
-//   return pads;
-// }
 var emulator = new Emulator(
   document.querySelector("#canvas"),
   null,
   new DosBoxLoader(
     DosBoxLoader.emulatorJS("emulators/em-dosbox/dosbox-sync.js"),
     DosBoxLoader.locateAdditionalEmulatorJS(locateAdditionalFiles),
-    // DosBoxLoader.nativeResolution(640, 480),
+    DosBoxLoader.nativeResolution(640, 480),
     DosBoxLoader.mountZip("c", DosBoxLoader.fetchFile("Game File","kq4.zip")),
     DosBoxLoader.startExe("sierra.exe"),
   )  
 )
 
 startButton.onclick = () => {
-  emulator.start();
+  emulator.start({ hasCustomCSS: true });
 }
 
 fullScreenButton.onclick = () => {
   canvas.requestFullscreen()
-  // document.body.requestFullscreen()
-  // canvas.style.margin="100px"
-  // canvas.style.width="75%"
-  // canvas.style.height="100%"
-  
 }
 
 canvas.addEventListener("keydown",function(e){
